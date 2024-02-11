35b9b1fa7dc2c5b63facba9347d2b4edf69206f2
Priyans4475 <priyanshukunar007@gmail.com>
2024-02-11 12:35:59 +0530

    2nd commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,51 @@
 const express=require('express');
 const userRouter=require('./index')
 const router=express.Router();
+const jwt=require('jsonwebtoken')
+const zod=require('zod');
+require("dotenv").config();
 
-router.post("/signup",);
+const {User}=require('../db');
+// const {JWT_SECRET} = require('../config.env');
+
+const signupSchema = zod.object({
+    username: zod.string().email(),
+    password:zod.string(),
+    firstname:zod.string(),
+    lastname:zod.string()
+  });
+
+
+router.post("/signup",async(req,res,next)=>{
+    const body=req.body;
+    const {success}=signupSchema.safeParse(body);
+    if(!success )
+    {
+        return res.json({
+            message:'user already exist'
+        })
+    }
+
+    const user=User.findOne({
+        username:body.username
+    })
+
+    if(user._id){
+        return res.json({
+            message:'user already exist'
+        })
+    }
+
+    const dbUser=await User.create(body);
+    const token=jwt.sign({
+        userId:dbUser._id
+    },process.env.JWT_SECRET)
+
+    res.json({
+        msg:'user created successfully',
+        token:token
+    })
+    
+});
 
 module.exports=router;
