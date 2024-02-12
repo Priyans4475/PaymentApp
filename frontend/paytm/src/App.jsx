44d12180f9bd9307e0395d575a1e625dbe591e50
Priyans4475 <priyanshukunar007@gmail.com>
2024-02-13 03:37:48 +0530

    frontend card on 13th feb

--- frontend/paytm/src/App.jsx
+++ frontend/paytm/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Sendmoney from './components/Sendmoney'
+
+import Register from './components/Register'
+import Login from './components/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+<Router>
+  <Register/>
+  <Routes>
+    <Route path='/signup' element={<Register/> }/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+    <Route path='/signin' element={<Login/>}/>
+    <Route path='/sendmoney' element={<Sendmoney/>}  />
+    
+  </Routes>
+
+</Router>
+   
   )
 }
 
---
+++ frontend/paytm/src/components/Sendmoney.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sendmoney = () => {
+  return (
+    <div>Sendmoney</div>
+  )
+}
+
+export default Sendmoney
